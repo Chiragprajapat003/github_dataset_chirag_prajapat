ae68c25ea10343608b690570bee3f510d3b97186
Chirag prajapat <kachholi0@gmail.com>
2026-06-05 17:00:14 +0530

    feat: wire Joi validation into auth and dataset routes, add data import npm scripts

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const authController = require('../controllers/authController');
+const validate = require('../middlewares/validate');
+const { registerSchema, loginSchema } = require('../validations/schemas');
 
 const router = express.Router();
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
+router.post('/register', validate(registerSchema), authController.register);
+router.post('/login', validate(loginSchema), authController.login);
 
 module.exports = router;
